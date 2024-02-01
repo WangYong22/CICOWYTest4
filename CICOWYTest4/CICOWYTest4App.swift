c7863104eb9229c419593ed70253d15590146392
WangYong22 <102638006+WangYong22@users.noreply.github.com>
2024-02-01 11:13:31 +0800

    Initial Commit

---
+++ CICOWYTest4/CICOWYTest4App.swift
@@ -0,0 +1,17 @@
+//
+//  CICOWYTest4App.swift
+//  CICOWYTest4
+//
+//  Created by wy on 2024/2/1.
+//
+
+import SwiftUI
+
+@main
+struct CICOWYTest4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
